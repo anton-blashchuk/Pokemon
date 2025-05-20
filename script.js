489a32d7a44a2f2b6a0f98bf58056bbaf673580b
Anton Blashchuk <anton.blashchuk@gmail.com>
2025-05-20 10:20:30 +0200

    cleanup; added comments; improved include function to not iterate over everything but filter better

--- script.js
+++ script.js
@@ -1,24 +1,4 @@
-async function fetchPokemonList(callback) {
-    try {
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=20");
-        const data = await response.json();
-        let pokemon
-        let pokemonDetails
-        const results = data.results;
-        let pokemonsDetailed = []
-        for (let i = 0; i < results.length; i++) {
-            pokemon = results[i]
-            pokemonDetails = await fetchPokemonDetails(pokemon.url);
-            let pokemonDetailed = {}
-            Object.assign(pokemonDetailed, pokemon, pokemonDetails)
-            pokemonsDetailed.push(pokemonDetailed)
-        }
-        callback(pokemonsDetailed)
-    } catch (error) {
-        console.error("Error fetching Pokémon list:", error);
-    }
-}
-
+/* fetches details for specific pokemon, which is chosen by URL in parameters */
 async function fetchPokemonDetails(pokemonUrl) {
     try {
         const response = await fetch(pokemonUrl);
@@ -29,34 +9,62 @@ async function fetchPokemonDetails(pokemonUrl) {
     }
 }
 
+/* fetches full Pokemon list and adds details for each one by an extra request per Pokemon */
+async function fetchPokemonList(callback) {
+    try {
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=20");
+        const data = await response.json();
+        const results = data.results;
+        let pokemon, pokemonDetails
+        let pokemonsDetailed = [] //list to return
+        for (let i = 0; i < results.length; i++) {
+            pokemon = results[i] //currently handled pokemon
+            pokemonDetails = await fetchPokemonDetails(pokemon.url);
+            let pokemonDetailed = {} //combined info from main list and extra response
+            Object.assign(pokemonDetailed, pokemon, pokemonDetails)
+            pokemonsDetailed.push(pokemonDetailed)
+        }
+        callback(pokemonsDetailed)
+    } catch (error) {
+        console.error("Error fetching Pokémon list:", error);
+    }
+}
 
+
+
+/* replaces elements with name `include` and `w3-include-html` attribute with HTML text fetched by value of this attribute */
 function includeHTML() {
-    var z, i, elmnt, file, xhttp;
-    /*loop through a collection of all HTML elements:*/
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-        elmnt = z[i];
-        /*search for elements with a certain attribute:*/
-        file = elmnt.getAttribute("w3-include-html");
-        if (file) {
-            /*make an HTTP request using the attribute value as the file name:*/
+    let allElements, element, includeAttribute, xhttp;
+    /*loop through a collection of HTML elements with name `include`*/
+    allElements = document.getElementsByName("include");
+    for (let i = 0; i < allElements.length; i++) {
+        element = allElements[i];
+        /*search for elements with a certain attribute*/
+        includeAttribute = element.getAttribute("w3-include-html");
+        if (includeAttribute) {
+            /*make an HTTP request using the attribute value as the file name*/
             xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function () {
                 if (this.readyState == 4) {
-                    if (this.status == 200) { elmnt.innerHTML = this.responseText; }
-                    if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
-                    /*remove the attribute, and call this function once more:*/
-                    elmnt.removeAttribute("w3-include-html");
+                    if (this.status == 200) { element.innerHTML = this.responseText; }
+                    if (this.status == 404) { element.innerHTML = "Page not found."; }
+                    /*remove the attribute*/
+                    element.removeAttribute("w3-include-html");
+                    /*call this function once more to handle next include*/
                     includeHTML();
                 }
             }
-            xhttp.open("GET", file, true);
+            xhttp.open("GET", includeAttribute, true);
             xhttp.send();
-            /*exit the function:*/
+            /*exit the function, updated document is handled by recursive call above*/
             return;
         }
     }
 };
 
 includeHTML()
-fetchPokemonList((pokemonList) => { console.log(pokemonList) })
+fetchPokemonList((pokemonList) => { 
+    console.log(pokemonList)
+    const pokemonContainer = document.getElementById("pokemon_container")
+    //TODO add all pokemons from list
+ })
